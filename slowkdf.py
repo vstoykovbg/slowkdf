55ca5eea609e1f9b77b92faa4cbdff6832cbd813
vstoykovbg <vstoykovbg@users.noreply.github.com>
2014-11-14 00:10:31 +0200

    Create slowkdf.py

---
+++ slowkdf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+
+import scrypt
+import binascii
+import getpass
+
+def SlowKDF(password, salt, max_counter):
+    counter = 0
+    digest = password
+    while counter < max_counter:
+      counter += 1
+      print "Iteration %s from %s..." % (counter, max_counter)
+      digest = scrypt.hash(digest, salt, N = 1048576, r = 8, p = 1, buflen = 128)
+    return digest
+
+mypass = getpass.getpass()
+mysalt = raw_input("Salt: ")
+mynumber = int(input("Number of iterations: "))
+
+mydigest = SlowKDF(mypass, mysalt, mynumber)
+
+print "\n\nDigest in hex format:", binascii.b2a_hex(mydigest)
+
+print "\n\nDigest in base64 format:", binascii.b2a_base64(mydigest)
