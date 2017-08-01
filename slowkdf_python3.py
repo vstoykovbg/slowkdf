0de0755765e238bbb38d3477f4d146c8d8d20dce
vstoykovbg <vstoykovbg@users.noreply.github.com>
2017-08-01 03:44:25 +0300

    adding sha512 just in case
    Also using range()

--- slowkdf_python3.py
+++ slowkdf_python3.py
@@ -3,13 +3,12 @@
 import scrypt
 import binascii
 import getpass
+from hashlib import sha512
 
-def SlowKDF(password, salt, max_counter):
-    counter = 0
+def SlowKDF(password, salt, i):
     digest = password
-    while counter < max_counter:
-        counter += 1
-        print ("Iteration %s from %s..." % (counter, max_counter) )
+    for counter in range(i):
+        print ("Iteration %s from %s..." % (counter+1, i) )
         digest = scrypt.hash(digest, salt, N = 1048576, r = 8, p = 1, buflen = 128)
     return digest
 
@@ -26,8 +25,21 @@ if mypass != getpass.getpass("Repeat passphrase (again): "):
 mysalt = input("Salt: ")
 mynumber = int(input("Number of iterations: "))
 
+mypass = mypass.encode()
+mysalt = mysalt.encode()
+
 mydigest = SlowKDF(mypass, mysalt, mynumber)
 
-print ("\n\nDigest in hex format:", binascii.b2a_hex(mydigest))
+print ("\n == Version 1 ==")
 
-print ("\n\nDigest in base64 format:", binascii.b2a_base64(mydigest))
+print ("\n\nDigest in hex format:", binascii.b2a_hex(mydigest).decode("utf-8"))
+
+print ("\n\nDigest in base64 format:", binascii.b2a_base64(mydigest).decode("utf-8"))
+
+print ("\n == Version 2 ==")
+
+mydigest_v2 = sha512(mypass+mysalt+mydigest).digest()
+
+print ("\n\nVersion 2 digest in hex format:", binascii.b2a_hex(mydigest_v2).decode("utf-8"))
+
+print ("\n\nVersion 2 digest in base64 format:", binascii.b2a_base64(mydigest_v2).decode("utf-8"))
